7fdedc316ca81d1c2cb311c2edac74eb90aba203
Giancarlos Salas <giansalex@gmail.com>
2019-05-30 18:01:55 -0500

    disable SSL verify

--- src/Bot/Request/CookieRequest.php
+++ src/Bot/Request/CookieRequest.php
@@ -36,7 +36,7 @@ class CookieRequest
     {
         $curl = new Curl();
 //        $curl->setOpt(CURLOPT_PROXY, '127.0.0.1:8888');
-//        $curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
+        $curl->setOpt(CURLOPT_SSL_VERIFYPEER, false);
         $curl->setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
         if (!empty($this->cookies)) {
             $curl->setCookies($this->cookies);
